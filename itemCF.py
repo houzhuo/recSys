aca2d012f8bdbc86313942734b56acbe016e1a53
houzhuo.cs <houzhuo.cs1994@yahoo.com>
2017-11-19 22:04:43 +0900

    def itemsim

--- itemCF.py
+++ itemCF.py
@@ -5,9 +5,31 @@
 @file: itemCF.py
 @time: 17/11/17 下午11:58
 """
-
+import pandas as pd
 def ItemSimilarity():
 
     data = pd.read_csv('movieData/data.csv')
     X = data['user_id']
     Y = data['movie_id']
+    user_item = dict()
+
+    for i in range(X.count()):
+        user = X.iloc[i]
+        item = Y.iloc[i]
+        if user not in user_item:
+            user_item[user] = set()
+        user_item[user].add(item)
+
+
+    C = dict()
+    N = dict()
+    for users, items in user_item.items():
+        for u in items:
+            N.setdefault(u,0)
+            N[u] += 1
+            C.setdefault(u,{})
+            for v in items:
+                if u == v:
+                    continue
+                C[u].setdefault(v,0)
+                C[u][v] += 1
